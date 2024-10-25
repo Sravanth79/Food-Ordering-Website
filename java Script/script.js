38b785844b8ccf4b4d6810a2db437f03e1dedcd5
sravantGitPractice <tsrabantkumartsk9145@gmail.com>
2024-10-25 11:18:55 +0530

    after update with assects and index file

---
+++ java Script/script.js
@@ -0,0 +1,52 @@
+let b1= document.querySelector("#b1");
+let b2= document.querySelector("#b2");
+let b3= document.querySelector("#b3");
+let b4= document.querySelector("#b4");
+
+b1.addEventListener(onclick,function(){
+    let price1=190
+})
+b2.addEventListener(onclick,function(){
+    let price2=200
+})
+b3.addEventListener(onclick,function(){
+    let price3=120
+})
+b4.addEventListener(onclick,function(){
+    let price4=200
+})
+console.log(price1);
+console.log(price2);
+console.log(price3);
+console.log(price4);
+
+
+
+// modebtn.addEventListener("click",()=>{
+//    if (mode==="light"){
+//     mode="dark";
+//     document.querySelector("body").style.backgroundColor="white";
+//     }
+//     else{
+//         mode="light";
+//     document.querySelector("body").style.backgroundColor="black";
+
+//     }
+//    console.log(mode);
+// })
+// let div= document.querySelector("div");
+// div.mouseover= ()=>{
+//      console.log("yiur are inside the div")
+// }
+// //this is copiolot code
+// document.getElementById('btn1').addEventListener('mouseover', function() {
+//     document.getElementById('btn1').style.backgroundColor = 'red'; // Change 'red' to any color you want
+// });
+
+// document.getElementById('btn1').addEventListener('mouseout', function() {
+//     document.getElementById('btn1').style.backgroundColor = 'blue'; // Change 'initial' to the original color of the div
+// });
+// let btn2=document.QuerySelector("btn2");
+// btn2.addEventListener("mouseover",function(){
+     
+// })
